01a551cdbc60b6483cd36e61e991306c6eaf682f
castella <carissa2206@gmail.com>
2026-03-07 15:37:32 +0000

    Flask token business boo

--- app.py
+++ app.py
@@ -1,23 +1,67 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-gigs = [
-    {"id": 1, "title": "Pipe fix", "price": 50, "seller": "Alice"},
-    {"id": 2, "title": "Painting nursery", "price": 100, "seller": "Bobbi"}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///capycure.db'
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
-@app.route('/api/gigs', methods=['GET'])
-def get_gigs():
-    return jsonify(gigs)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
 
-@app.route('/api/gigs', methods=['POST'])
-def add_gig():
-    new_data = request.json
-    gigs.append(new_data)
-    return jsonify({"message": "Gig added!"}), 201
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    if User.query.filter_by(username=data['username']).first():
+        return jsonify({"message": "User already exists"}), 400
+
+    hashed_pw = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], password_hash=hashed_pw)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created!"}), 201
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+
+    if user and check_password_hash(user.password_hash, data['password']):
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"token": access_token, "username": user.username}), 200
+
+    return jsonify({"message": "Invalid credentials"}), 401
+
+@app.route('/debug/users', methods=['GET'])
+def list_users():
+    users = User.query.all()
+
+    user_list = [{"id": u.id, "username": u.username, "pw_hash": u.password_hash} for u in users]
+    return jsonify(user_list)
+
+@app.route('/api/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    return jsonify({"username": user.username, "id": user.id})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
