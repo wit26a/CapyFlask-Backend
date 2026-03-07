4e1f03b437778ae43d18b51e705a1c6c2ab47bb2
castella <carissa2206@gmail.com>
2026-03-07 14:49:33 +0000

    Getting things connected

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+gigs = [
+    {"id": 1, "title": "Pipe fix", "price": 50, "seller": "Alice"},
+    {"id": 2, "title": "Painting nursery", "price": 100, "seller": "Bobbi"}
+]
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/gigs', methods=['GET'])
+def get_gigs():
+    return jsonify(gigs)
 
+@app.route('/api/gigs', methods=['POST'])
+def add_gig():
+    new_data = request.json
+    gigs.append(new_data)
+    return jsonify({"message": "Gig added!"}), 201
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5001, debug=True)
